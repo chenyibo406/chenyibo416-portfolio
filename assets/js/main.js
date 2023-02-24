78a4ca93deb3bddb70972e519119b54e80de7ef0
AC <chenyibo416@gmail.com>
2023-02-24 20:34:50 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+/* ===== MENU SHOW ===== */
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/* ===== Active And Remove Menu ===== */
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  // Active link
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  //   Remove mene mobile
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ===== Scroll Reveal Animation ===== */
+
+/* Scroll home */
+/* Scroll about */
+/* Scroll Skills */
+/* Scroll Work */
+/* Scroll CONTINUE */
